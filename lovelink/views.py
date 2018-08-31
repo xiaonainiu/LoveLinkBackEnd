56e0d897ddbc79d8504c6a0f47dad55475976938
xiaonainiu <es930119@gmail.com>
2018-08-31 13:23:42 +1000

    update

--- lovelink/views.py
+++ lovelink/views.py
@@ -138,17 +138,17 @@ def personInfoOut(request):
     else:
         return HttpResponse(False)
 
-def transction(text):
+def transction(text,name):
   w3=Web3(Web3.HTTPProvider('https://mainnet.infura.io/9dhHYFuxJixnXwEdnwIy '))#连接到Eth的远程节点
   priv_key = env_dist['BLOCK_KEY'] #爱链的eth账户的私钥
-  account = Account.privateKeyToAccount(priv_key) #通过私钥得到公钥也就是账户地址
-  nonce = w3.eth.getTransactionCount(account.address) #通过返回指定地址发起的交易数，得到防止重放攻击的数字
-  data=Web3.toHex(str.encode(text))#交易附加的信息，需要将字符串转换为16进制编码，需要前端传递来需要保存的数据
+  account = Account.privateKeyToAyToAccount(priv_key) #通过私钥得到公钥也就是账户地址
+  nonce = w3.eth.getTransactioctionCount(account.address) #通) #通过返回指定地址发起的交易数，得到防止重放攻击的数字
+  data=Web3.toHex(str.encode(text+'--'+name))#交易附加的信息，需要将字符串转换为16进制编码，需要前端传递来需要保存的数据
   payload = {
-    'to': env_dist['BLOCK_KEY'],
+    'to':  '0x8Fe2Af03Ed1d362371261AB33C400F24fBB82D8f',
     'value': 0,
     'gas': 200000,           #运算步数的上限
-    'gasPrice': Web3.toWei(10,'gwei'),#每一步运算耗费的Eth
+    'gasPrice': Web3.toWei(6,'gwei'),#每一步运算耗费的Eth
     'nonce': nonce,
     'data':data
   }
