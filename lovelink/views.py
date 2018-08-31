def1a190b4daec30c5fbc787fd5e8c3bb0158438
xiaonainiu <es930119@gmail.com>
2018-08-31 13:53:40 +1000

    update

--- lovelink/views.py
+++ lovelink/views.py
@@ -142,7 +142,7 @@ def personInfoOut(request):
 def transction(text,name):
   w3=Web3(Web3.HTTPProvider('https://mainnet.infura.io/9dhHYFuxJixnXwEdnwIy '))#连接到Eth的远程节点
   priv_key = env_dist['BLOCK_KEY'] #爱链的eth账户的私钥
-  account = Account.privateKeyToAyToAccount(priv_key) #通过私钥得到公钥也就是账户地址
+  account = Account.privateKeyToAccount(priv_key) #通过私钥得到公钥也就是账户地址
   nonce = w3.eth.getTransactioctionCount(account.address) #通) #通过返回指定地址发起的交易数，得到防止重放攻击的数字
   data=Web3.toHex(str.encode(text+'--'+name))#交易附加的信息，需要将字符串转换为16进制编码，需要前端传递来需要保存的数据
   payload = {
