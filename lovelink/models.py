3cec4bfb815d5901e6b918b2f4bf5cf7bcb37e3f
yu zhicheng <40519530+davidsram@users.noreply.github.com>
2018-09-11 09:05:11 +0800

    Update models.py

--- lovelink/models.py
+++ lovelink/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from mongoengine import *
 # Create your models here.
-class Poem(Document):
-# poem 
+class Oath(Document):
+# oath 
 meta = { 
   # 数据库中显示的名字 '
-  collection': 'poem_data' } 
-  poem_id = SequenceField(required=True, primary_key=True)
-  author = StringField()
-  title = StringField() # 可以定义查询集
+  collection': 'oath_data' } 
+  openid = SequenceField(required=True, primary_key=True)
+  username = StringField()
+  text= StringField()
+  oathTitle=StringField()
+  image=StringField()
+  time=StringField()
+  avatarUrl=StringField()
+  tx_hash=StringField()# 可以定义查询集
   @queryset_manager def show_newest(doc_cls, queryset): 
   # 通过poem_id降序显示
-  return queryset.order_by('-poem_id') 
+  return queryset.order_by('-openid') 
 
 
